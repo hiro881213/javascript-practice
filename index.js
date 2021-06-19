1bb1da53c95cb3228113294e8bb34efd2ecd11c7
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-19 19:13:42 +0900

    アロー関数まとめ

--- index.js
+++ index.js
@@ -18,7 +18,24 @@ console.log(`${human.name}(${human.age} ) ${human.address}`);
  * 配列もconstでは変更できる
  */
 const arr = ['dog','cat'];
-alert(arr);
+console.log(arr);
 
 arr.push('monkey');
-alert(arr);
+console.log(arr);
+
+/**
+ * アロー関数
+ */
+
+const func1 = (str) => {
+    return str;
+}
+
+console.log(func1("TEST"));
+
+// アロー関数省略系
+const func2 = str => str;
+console.log(func2("TEST2"));
+
+const func3 = (num1,num2) => num1+num2;
+alert(func3(3,2));
