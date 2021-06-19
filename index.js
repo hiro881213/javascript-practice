251170e3b9513f6acdb29ff835d07817e3c27f4d
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-19 19:24:51 +0900

    分割代入まとめ

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-/*
- * オブジェクト型はconst型でも変更できる
- */
-const human = {
-    name:"A太郎",
-    age:29
-};
+// /*
+//  * オブジェクト型はconst型でも変更できる
+//  */
+// const human = {
+//     name:"A太郎",
+//     age:29
+// };
 
-console.log(`${human.name}(${human.age})`)
+// console.log(`${human.name}(${human.age})`)
 
-human.name = "B太郎";
-console.log(`${human.name}(${human.age})`)
+// human.name = "B太郎";
+// console.log(`${human.name}(${human.age})`)
 
-human.address = 'Tokyo';
-console.log(`${human.name}(${human.age} ) ${human.address}`);
+// human.address = 'Tokyo';
+// console.log(`${human.name}(${human.age} ) ${human.address}`);
 
 /**
  * 配列もconstでは変更できる
@@ -38,4 +38,22 @@ const func2 = str => str;
 console.log(func2("TEST2"));
 
 const func3 = (num1,num2) => num1+num2;
-alert(func3(3,2));
+console.log(func3(3,2));
+
+/**
+ * 分割代入
+ */
+const myProfile ={
+    name:'Hiro',
+    age:32
+};
+
+const {name, age} = myProfile;
+console.log(`私の名前は${name}です。${age}歳です`);
+
+// 配列の分割代入
+const arrMyProfile = ['Sam',23];
+const [user, ages] = arrMyProfile;
+
+alert(`私の名前は${user}です。${ages}歳です`);
+
