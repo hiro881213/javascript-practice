d3f0ca07b220e920d1f921d63c33c2edd44466d6
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-20 18:56:39 +0900

    三項演算子練習

--- index.js
+++ index.js
@@ -107,3 +107,23 @@ console.log(newNumArr);
 
 // 順番を取得する
 nameArr2.map((name,index) => console.log(`${index+1}番目は${name}です。`));
+
+/**
+ * 三項演算子
+ */
+
+const val1 = 1 > 0 ? 'trueです': 'falseです';
+console.log(val1);
+
+const num = 'AAA';
+// toLocaleString: カンマ区切りにしてくれる関数
+const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+
+console.log(formattedNum);
+
+const checkSum = (num1,num2) => {
+    return num1+num2 > 100 ? '100を超えています': '許容範囲内です';
+}
+
+alert(checkSum(10,20));
+alert(checkSum(100,30));
