91f4be5f43e681d44a0e5a095316d6e72a993cdb
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-20 17:56:57 +0900

    スプレット構文練習

--- index.js
+++ index.js
@@ -60,5 +60,28 @@ console.log(`私の名前は${user}です。${ages}歳です`);
 /**
  * デフォルト値
  */
-const sayHello = (User='ゲスト') => alert(`こんにちは${User}さん`);
+const sayHello = (User='ゲスト') => console.log(`こんにちは${User}さん`);
 sayHello();
+
+/**
+ * スプレット構文
+ */
+// 配列の展開
+const arr1 = [1,2];
+const sumFunk  = (num1,num2) => console.log(num1+num2);
+sumFunk(...arr1);
+
+// まとめる
+const arr2 =[1,2,3,4,5];
+const [num1,num2,...arr3] = arr2;
+
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10,20];
+const arr5 = [30,40];
+
+const arr7 = [...arr4,...arr5];
+
+console.log(arr7);
+
